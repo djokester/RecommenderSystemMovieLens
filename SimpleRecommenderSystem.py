4fc99664891ac545b025b2d3b33eb1545152e231
djokester <samriddhidjokestersinha@gmail.com>
2016-12-26 12:41:51 +0530

    Simple Recommender System Completed

--- SimpleRecommenderSystem.py
+++ SimpleRecommenderSystem.py
@@ -11,9 +11,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #column headers for the dataset
-data_cols = ['user id','item id','rating','timestamp']
+data_cols = ['user id','movie id','rating','timestamp']
 item_cols = ['movie id','movie title','release date','video release date','IMDb URL','unknown','Action','Adventure','Animation','Childrens','Comedy','Crime','Documentary','Drama','Fantasy','Film-Noir','Horror','Musical','Mystery','Romance ','Sci-Fi','Thriller','War' ,'Western']
-user_cols = ['id','age','gender','occupation','zip code']
+user_cols = ['user id','age','gender','occupation','zip code']
 
 #importing the data files onto dataframes 
 users = pd.read_csv('Desktop/ml-100k/u.user', sep='|', names=user_cols, encoding='latin-1')
@@ -29,7 +29,30 @@ print("\nData Head")
 print(data.head())
 
 #printing the details of these dataframes() 
-print(users.info())
-print(item.info())
-print(data.info())
+print('\n',users.info())
+print('\n',item.info())
+print('\n',data.info())
+
+#Using Pandas to manipulate the data 
+
+#Create one data frame from the three
+dataset = pd.merge(pd.merge(item, data),users)
+print(dataset.head())
+
+#Create dataframes with mean rating and total number of ratings
+ratings_total = dataset.groupby('movie title').size()
+
+rt = (dataset.groupby('movie title'))['movie title','rating']
+ratings_mean = (rt.mean())
+
+#modify those dataframes so that we can merge the two 
+ratings_total = pd.DataFrame({'movie title':ratings_total.index, 'total ratings': ratings_total.values})
+ratings_mean['movie title'] = ratings_mean.index
+
+final = pd.merge(ratings_mean, ratings_total).sort_values(by = 'total ratings', ascending= False)
+print(final.head())
+print(final.describe())
+
+final = final[:300].sort_values(by = 'rating', ascending = False)
+print(final.head())
 
